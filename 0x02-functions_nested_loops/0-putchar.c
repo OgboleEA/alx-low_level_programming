ccf2ba4e56087efd18d6b58cf4bdf21223246df1
OgboleEA <eaogbole@gmail.com>
2022-09-14 16:37:33 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always return 0
+ * main - print putchar
+ * Return: always 0
  */
+
 int main(void)
 {
-	_putchar('Hello World');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
-		return (0);
-}
+	return (0);
 
+}
