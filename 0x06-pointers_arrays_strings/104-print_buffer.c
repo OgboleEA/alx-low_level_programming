19b9c63b16bccf2ffeb0719034ec9e52a2f6e6ac
OgboleEA <eaogbole@gmail.com>
2022-09-22 17:07:42 -0700

    Task 104

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -12,14 +12,12 @@
 void print_buffer(char *b, int size)
 {
 	 int byte, index;
-	 
+
 	 for (byte = 0; byte < size; byte += 10)
 	 {
 		 printf("%08x: ", byte);
-		 
-		 for (index = 0; index < 10; index++)
-		 {
-			 if ((index + byte) >= size)
+
+		 if ((index + byte) >= size)
 				 printf(" ");
 
 			 else
