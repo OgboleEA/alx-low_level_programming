3d67f45144782b4272156629d89b98fa7dc7ec35
OgboleEA <eaogbole@gmail.com>
2022-09-28 09:15:39 -0700

    Task2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,17 +2,20 @@
 
 /**
  * _strlen_recursion - returns the length of a string
- * @*s: string
+ * @s: string
  *
- * Return: int
+ * Return: length of string
  */
 
 int _strlen_recursion(char *s)
 {
-	int len;
+	int c;
 
+	c = 0;
 	if (*s == '\0')
 		return (0);
-	len = _strlen_recursion(s + 1);
-	return (len + 1);
+
+	c = 1 +  _strlen_recursion(s + 1);
+
+	return (c);
 }
