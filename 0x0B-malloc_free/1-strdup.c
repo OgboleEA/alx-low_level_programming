56e9ba2fcb7c55dad9bfb91b1aea4a6138debac1
OgboleEA <eaogbole@gmail.com>
2022-10-04 22:24:25 -0700

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,18 +11,24 @@
 
 char *_strdup(char *str)
 {
-	char *sd;
-	unsigned int size = 0, i;
+	char *duplicate;
+	int index, len = 0;
 
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-		size++;
-	sd = malloc(sizeof(char) * (size + 1));
-	if (sd == NULL)
-		eturn (NULL);
-	for (i = 0; i < size; i++)
-		sd[i] = str[i];
-	sd[size] = '\0';
-	return (sd);
+
+	for (index = 0; str[index]; index++)
+		len++;
+
+	duplicate = malloc(sizeof(char) * (len + 1));
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		duplicate[index] = str[index];
+
+	duplicate[len] = '\0';
+
+	return (duplicate);
 }
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,11 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * strtow - string to words
  * @str: string to go to words
  * Return: char value
  */
+
+int _putchar(char c);
+void *malloc(size_t size);
+void free(void *ptr);
 char *str_concat(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
